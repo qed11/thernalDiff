15ee3c12b7addf1218c4c4f375c4e8a7dca9ebeb
eqd11 <ericdai1130@gmail.com>
2021-03-11 19:33:00 -0500

    commit on 'main;

--- thernalDiff5/helpers.py
+++ thernalDiff5/helpers.py
@@ -1,2 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy as scipy
 
 # helper file for helpful functions
+
+def reader(filename:str):
+    f = open(filename,'r')
+    lines = np.asarray(f.readlines())
+    f.close()
+    time = []
+    temp = []
+    for i in lines:
+        t,T,stamp = i.split('\t')
+        try:
+            time.append(float(t))
+            temp.append(float(T))
+        except:
+            continue
+    return np.asarray(time), np.asarray(temp)
+
